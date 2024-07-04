2831b5c30153aeedf1003c9466018e46ccf0faa3
mrjamiebowman <mrjamiebowman@gmail.com>
2024-07-03 21:50:36 -0500

    orders/customers

--- build.ps1
+++ build.ps1
@@ -7,17 +7,29 @@ $NUGET_PAT = [System.Environment]::GetEnvironmentVariable('NUGET_PAT','user')
 # docker build
 $VERSION = 'latest'
 
-# # customers api
-# docker build --no-cache -f "CustomersApi\Dockerfile" `
-#     --build-arg NUGET_USER=$NUGET_USER --build-arg NUGET_PAT=$NUGET_PAT `
-#     --label "company=mrjamiebowman" `
-#     -t mrjamiebowman/app-k8s-customer-api:$VERSION .
-
-# identity server
-docker build --no-cache -f "IdentityServer\Dockerfile" `
+# customers api
+docker build --no-cache -f "CustomersApi\Dockerfile" `
     --build-arg NUGET_USER=$NUGET_USER --build-arg NUGET_PAT=$NUGET_PAT `
     --label "company=mrjamiebowman" `
-    -t mrjamiebowman/app-k8s-identityserver:$VERSION .
+    -t mrjamiebowman/app-k8s-customer-api:$VERSION .
+
+# customers consumer
+docker build --no-cache -f "CustomersConsumer\Dockerfile" `
+    --build-arg NUGET_USER=$NUGET_USER --build-arg NUGET_PAT=$NUGET_PAT `
+    --label "company=mrjamiebowman" `
+    -t mrjamiebowman/app-k8s-customers-consumer:$VERSION .
+
+# # orders api
+# docker build --no-cache -f "OrdersApi\Dockerfile" `
+#     --build-arg NUGET_USER=$NUGET_USER --build-arg NUGET_PAT=$NUGET_PAT `
+#     --label "company=mrjamiebowman" `
+#     -t mrjamiebowman/app-k8s-orders-api:$VERSION .
+
+# # identity server
+# docker build --no-cache -f "IdentityServer\Dockerfile" `
+#     --build-arg NUGET_USER=$NUGET_USER --build-arg NUGET_PAT=$NUGET_PAT `
+#     --label "company=mrjamiebowman" `
+#     -t mrjamiebowman/app-k8s-identityserver:$VERSION .
 
 # display built images
 docker images | findstr mrjamiebowman 
--- push.ps1
+++ push.ps1
@@ -1,13 +1,13 @@
 Clear-Host
 
-# # api: customers
-# docker push mrjamiebowman/app-k8s-customer-api:latest
+# api: customers
+docker push mrjamiebowman/app-k8s-customer-api:latest
+
+# consumers: customers
+docker push mrjamiebowman/app-k8s-customer-consumers:latest
 
 # # api: orders
 # docker push mrjamiebowman/app-k8s-orders-api:latest
 
-# # consumers: customers
-# docker push mrjamiebowman/app-k8s-customer-consumers:latest
-
-# identityserver
-docker push mrjamiebowman/app-k8s-identityserver:latest
+# # identityserver
+# docker push mrjamiebowman/app-k8s-identityserver:latest
