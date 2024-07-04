ae7602cd909fafd1d1938fd74a67c5b6d9baa4f2
mrjamiebowman <mrjamiebowman@gmail.com>
2024-07-04 16:06:16 -0500

    updates

--- helm-template.ps1
+++ helm-template.ps1
@@ -11,32 +11,32 @@ if ($args[0] -eq 'dev') {
 }
 
 
-# # helm template (-f charts/values.prod.yaml)
-# if ($args[0] -eq 'dev') {
-#     helm template consumerscustomers .\CustomersConsumer\charts\consumer-customers\ -n mrjamiebowman -f CustomersConsumer/charts/consumer-customers/values.dev.yaml --debug
-# } elseif ($args[0] -eq 'prod') {
-#     helm template consumerscustomers .\CustomersConsumer\charts\consumer-customers\ -n mrjamiebowman -f CustomersConsumer/charts/consumer-customers/values.prod.yaml --debug
-# } else {
-#     helm template consumerscustomers .\CustomersConsumer\charts\consumer-customers\ -n mrjamiebowman --debug
-# }
+# helm template (-f charts/values.prod.yaml)
+if ($args[0] -eq 'dev') {
+    helm template consumerscustomers .\CustomersConsumer\charts\consumer-customers\ -n mrjamiebowman -f CustomersConsumer/charts/consumer-customers/values.dev.yaml --debug
+} elseif ($args[0] -eq 'prod') {
+    helm template consumerscustomers .\CustomersConsumer\charts\consumer-customers\ -n mrjamiebowman -f CustomersConsumer/charts/consumer-customers/values.prod.yaml --debug
+} else {
+    helm template consumerscustomers .\CustomersConsumer\charts\consumer-customers\ -n mrjamiebowman --debug
+}
 
 
-# # helm template (-f charts/values.prod.yaml)
-# if ($args[0] -eq 'dev') {
-#     helm template apiorders .\OrdersApi\charts\api-orders\ -n mrjamiebowman -f OrdersApi/charts/api-orders/values.dev.yaml --debug
-# } elseif ($args[0] -eq 'prod') {
-#     helm template apiorders .\OrdersApi\charts\api-orders\ -n mrjamiebowman -f OrdersApi/charts/api-orders/values.prod.yaml --debug
-# } else {
-#     helm template apiorders .\OrdersApi\charts\api-orders\ -n mrjamiebowman --debug
-# }
+# helm template (-f charts/values.prod.yaml)
+if ($args[0] -eq 'dev') {
+    helm template apiorders .\OrdersApi\charts\api-orders\ -n mrjamiebowman -f OrdersApi/charts/api-orders/values.dev.yaml --debug
+} elseif ($args[0] -eq 'prod') {
+    helm template apiorders .\OrdersApi\charts\api-orders\ -n mrjamiebowman -f OrdersApi/charts/api-orders/values.prod.yaml --debug
+} else {
+    helm template apiorders .\OrdersApi\charts\api-orders\ -n mrjamiebowman --debug
+}
 
 
-# # helm template (-f charts/values.prod.yaml)
-# if ($args[0] -eq 'dev') {
-#     helm template identityserver .\IdentityServer\charts\consumer-customers\ -n mrjamiebowman -f IdentityServer/charts/app-identityserver/values.dev.yaml --debug
-# } elseif ($args[0] -eq 'prod') {
-#     helm template identityserver .\IdentityServer\charts\consumer-customers\ -n mrjamiebowman -f IdentityServer/charts/app-identityserver/values.prod.yaml --debug
-# } else {
-#     helm template identityserver .\IdentityServer\charts\consumer-customers\ -n mrjamiebowman --debug
-# }
+# helm template (-f charts/values.prod.yaml)
+if ($args[0] -eq 'dev') {
+    helm template identityserver .\IdentityServer\charts\app-identityserver\ -n mrjamiebowman -f IdentityServer/charts/app-identityserver/values.dev.yaml --debug
+} elseif ($args[0] -eq 'prod') {
+    helm template identityserver .\IdentityServer\charts\app-identityserver\ -n mrjamiebowman -f IdentityServer/charts/app-identityserver/values.prod.yaml --debug
+} else {
+    helm template identityserver .\IdentityServer\charts\app-identityserver\ -n mrjamiebowman --debug
+}
 
