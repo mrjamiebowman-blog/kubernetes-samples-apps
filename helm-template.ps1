20614a187afa93ada9e846581159a50486da82a6
mrjamiebowman <mrjamiebowman@gmail.com>
2024-07-04 12:26:20 -0500

    working

--- helm-template.ps1
+++ helm-template.ps1
@@ -3,9 +3,9 @@ Clear-Host
 
 # helm template (-f charts/values.prod.yaml)
 if ($args[0] -eq 'dev') {
-    helm template apicustomers .\charts\api-customers\ -n sn -f charts/api-customers/values.dev.yaml --debug
+    helm template apicustomers .\CustomersApi\charts\api-customers\ -n sn -f CustomersApi/charts/api-customers/values.dev.yaml --debug
 } elseif ($args[0] -eq 'prod') {
-    helm template apicustomers .\charts\api-customers\ -n sn -f charts/api-customers/values.prod.yaml --debug
+    helm template apicustomers .\CustomersApi\charts\api-customers\ -n sn -f CustomersApi/charts/api-customers/values.prod.yaml --debug
 } else {
-    helm template apicustomers .\charts\api-customers\ -n sn --debug
+    helm template apicustomers .\CustomersApi\charts\api-customers\ -n sn --debug
 }
