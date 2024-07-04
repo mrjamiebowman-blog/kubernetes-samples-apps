0da33a213404f5676fd64c1d5ba0651ef7c6283b
mrjamiebowman <mrjamiebowman@gmail.com>
2024-07-03 20:51:13 -0500

    push

--- push.ps1
+++ push.ps1
@@ -1,4 +1,13 @@
 Clear-Host
 
-# api: customers
-docker push mrjamiebowman/app-k8s-customer-api:latest
+# # api: customers
+# docker push mrjamiebowman/app-k8s-customer-api:latest
+
+# # api: orders
+# docker push mrjamiebowman/app-k8s-orders-api:latest
+
+# # consumers: customers
+# docker push mrjamiebowman/app-k8s-customer-consumers:latest
+
+# identityserver
+docker push mrjamiebowman/app-k8s-identityserver:latest
